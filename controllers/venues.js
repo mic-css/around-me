9fa24668f0caecf30e6c265a7af83e36c21019d7
Mic <mic.cassano@gmail.com>
2016-06-12 13:29:45 +0100

    Extract url constructor methods to

--- controllers/venues.js
+++ controllers/venues.js
@@ -1,32 +1,28 @@
 var request = require('request');
+var async = require('async');
 var venueFactory = require('../models/venueFactory');
+var urlConstructor = require('../modules/urlConstructor');
 
 exports.getVenuesList = function (req, res) {
-  function constructUrl() {
-    var url = 'https://api.foursquare.com/v2/venues/search';
-    var coordinates = req.query.coordinates;
-    var token = process.env.API_TOKEN;
-    var version = process.env.API_VERSION;
-    var limit = 10;
-    var params = ('?ll=' + coordinates + '&limit=' + limit + '&oauth_token=' + token + '&v=' + version);
 
-    return url + params;
-  }
-
-  request.get(constructUrl(), function (err, response, body) {
+  request.get(urlConstructor.constructUrl(req), function (err, response, body) {
     if (err) {
       res.status(400).jsonp({'ERROR': err});
     } else {
-      var apiVenues = JSON.parse(body).response.venues;
       var venues = [];
+
+      var apiVenues = JSON.parse(body).response.venues;
+
+      var venueUrls = apiVenues.map(function (venue) {
+        return urlConstructor.constructVenueUrl(venue.id);
+      });
 
       apiVenues.forEach(function (apiVenue) {
         venues.push(venueFactory.createVenue(apiVenue));
       });
 
-      res.jsonp({
-        'venues': venues
-      });
+      res.jsonp({'venues': venues});
+
     }
   });
 };
---
+++ modules/urlConstructor.js
@@ -0,0 +1,21 @@
+// TODO: refactor into single
+
+exports.constructVenueUrl = function(id) {
+  var url = 'https://api.foursquare.com/v2/venues';
+  var token = process.env.API_TOKEN;
+  var version = process.env.API_VERSION;
+  var params = ('?oauth_token=' + token + '&v=' + version);
+
+  return url + '/' + id + params;
+};
+
+exports.constructUrl = function(req) {
+  var url = 'https://api.foursquare.com/v2/venues/search';
+  var coordinates = req.query.coordinates;
+  var token = process.env.API_TOKEN;
+  var version = process.env.API_VERSION;
+  var limit = 10;
+  var params = ('?ll=' + coordinates + '&limit=' + limit + '&oauth_token=' + token + '&v=' + version);
+
+  return url + params;
+};
