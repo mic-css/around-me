e9d3827745cc0c7637e79148cd8920a961812eae
Mic <mic.cassano@gmail.com>
2016-06-14 12:31:29 +0100

    Set up Angular

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var venues = require('./routes/venues');
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -30,7 +31,7 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/venues', venues);
 
 // catch 404 and forward to error handler
